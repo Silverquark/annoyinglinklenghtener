3909dbb9cbef2fb3fec012d0d8b043fa9b5f14c8
Leo Lüker <le.lueker@gmail.com>
2022-09-27 19:59:09 +0200

    asd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,24 +20,26 @@ const Home: NextPage = () => {
         <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
           Annoying Link Lengthener
         </h1>
-        <input onChange={(e) => {
-          setLink(e.target.value);
-          setIsCopied(false)
-        }} value={link} className='h-10 mb-4 p-4 rounded-xl w-1/2' placeholder="Paste your link here..." />
-        <button className="bg-slate-600 outline-2 rounded p-4 text-white mb-4" onClick={() => {
-          mutation.mutate({ url: link }, {
-            onSuccess: (data) => {
-              navigator.clipboard.writeText(`${window.location.href}${data}`)
-              setIsCopied(true);
-            }
-          });
+        <input
+          value={link}
+          className='h-10 mb-4 p-4 rounded-xl w-1/2'
+          placeholder="Paste your link here..."
+          onChange={(e) => {
+            setLink(e.target.value);
+            setIsCopied(false)
+          }} />
+        <button className="bg-slate-600 outline-2 rounded p-4 text-white mb-4"
+          onClick={() => {
+            mutation.mutate({ url: link }, {
+              onSuccess: async (data) => {
+                navigator.clipboard.writeText(`${window.location.href}${data}`)
+                setIsCopied(true);
+              }
+            });
 
 
-        }}>Make it longer!</button>
-
-
-
-          <p className="text-slate-800 text-xl text-ellipsis break-words">{isCopied ? `The link has been copied to your clipboard!` : ''}</p>
+          }}>Make it longer!</button>
+        <p className="text-slate-800 text-xl text-ellipsis break-words">{isCopied ? `The link has been copied to your clipboard!` : ''}</p>
 
       </main>
     </>
